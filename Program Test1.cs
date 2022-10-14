23fa553199bbe8aed1bd3a71627dfec16da43fdf
Henrik <102602537+Vargfor@users.noreply.github.com>
2022-10-14 12:34:46 +0200

    Add files via upload

---
+++ Program Test1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+using System.Collections;
+using System.IO;
+
+namespace Project1 { 
+    class Test1
+    {
+        static void Main(string[] args)
+        {
+            string genderA = "Man";
+            string genderB = "Woman";
+
+            Console.WriteLine("Input age;");
+            string age = Console.ReadLine();
+            Console.WriteLine("There where once a " + genderA + " and a " + genderB + " They where both " + age + " years old");
+            
+        }
+    }
+}
